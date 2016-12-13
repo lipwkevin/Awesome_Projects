916887fea51f89a2ff378e213a842f40ea5d5133
Kevin Li <lipwkevin@gmail.com>
2016-12-13 11:55:00 -0800

    undo javascript string injection

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module GPMTool
   class Application < Rails::Application
 
     config.time_zone = 'Pacific Time (US & Canada)'
-    config.public_file_server.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
