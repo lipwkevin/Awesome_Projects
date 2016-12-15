9f78f9c90c68cf76f737494f3b1b25ebdd4aaac0
Kevin Li <lipwkevin@gmail.com>
2016-12-15 15:40:55 -0800

    wrap up styling

--- app/models/message.rb
+++ app/models/message.rb
@@ -18,14 +18,6 @@ class Message < ApplicationRecord
     return created_at.strftime('%d %b %Y %H:%M')
   end
 
-  def getMessage(user)
-    if (sender_id==user.id.to_s)
-      return "#{body} << #{getSenderName}"
-    else
-      return "#{getSenderName} >> #{body}"
-    end
-  end
-
   def updateConversation
     conversation.update updated_at:DateTime.now
   end
